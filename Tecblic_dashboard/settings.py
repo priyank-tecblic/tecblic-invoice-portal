457fe402cdb54b2dcd0a43a715f6d4c52cab563a
priyank <priyankvyas.tecblic@gmail.com>
2023-01-16 17:48:27 +0530

    adding icon

--- Tecblic_dashboard/settings.py
+++ Tecblic_dashboard/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=25
