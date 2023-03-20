411cb3ebcc166391894430679bfab51220d3b109
priyank <priyankvyas.tecblic@gmail.com>
2023-03-20 14:58:21 +0530

    new changes

--- tecblicapp/utils.py
+++ tecblicapp/utils.py
@@ -1,5 +1,6 @@
 import os.path
 from io import BytesIO
+from django.http import FileResponse
 from django.http import HttpResponse
 from django.template.loader import get_template
 from xhtml2pdf import pisa
