5d032851582f65b8dbd1f806d9c25b14c4e089d1
priyank <priyankvyas.tecblic@gmail.com>
2023-03-24 18:05:36 +0530

    change git ignore

--- Tecblic_dashboard/settings.py
+++ Tecblic_dashboard/settings.py
@@ -152,4 +152,4 @@ EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=25
 EMAIL_USE_TLS=True
 EMAIL_HOST_USER='priyank.imscit18@gmail.com'
-EMAIL_HOST_PASSWORD='gasfvnaypfwnebkn'
+EMAIL_HOST_PASSWORD='leszowmupmbarkga'
